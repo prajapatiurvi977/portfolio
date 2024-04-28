9217d4a1e4a8176f9e84c9b5596223262be8d7c7
prajapatiurvi977 <prajapatiurvi977@gmail.com>
2024-04-28 14:55:25 -0400

    Set selected tab based on url param

--- src/components/work-detail/index.tsx
+++ src/components/work-detail/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import ArrowLeftCircle from '../../assets/images/arrow-left-circle.svg';
 import { ROUTES } from '../../navigation/constants';
 import type { ITabListProps } from '../tab-list';
@@ -9,18 +9,24 @@ import { TicTacToe } from './tic-tac-toe';
 import { TimHortons } from './tim-hortons';
 import { TorontoZoo } from './toronto-zoo';
 
+interface WorkDetailParams extends Record<PropertyKey, any> {
+  id: string;
+}
+
+const listConfig: ITabListProps['listConfig'] = [
+  { id: 'tim-hortons', title: 'TIM HORTONS', content: <TimHortons /> },
+  { id: 'toronto-zoo', title: 'TORONTO ZOO', content: <TorontoZoo /> },
+  {
+    id: 'humber-current',
+    title: 'HUMBER CURRENT',
+    content: <HumberCurrent />,
+  },
+  { id: 'tic-tac-toe', title: 'TIC TAC TOE', content: <TicTacToe /> },
+];
+
 const WorkDetail = () => {
-  const listConfig: ITabListProps['listConfig'] = [
-    { id: 'tim-hortons', title: 'TIM HORTONS', content: <TimHortons /> },
-    { id: 'toronto-zoo', title: 'TORONTO ZOO', content: <TorontoZoo /> },
-    {
-      id: 'humber-current',
-      title: 'HUMBER CURRENT',
-      content: <HumberCurrent />,
-    },
-    { id: 'tic-tac-toe', title: 'TIC TAC TOE', content: <TicTacToe /> },
-  ];
-  const initialSelectedTabId = listConfig[0].id;
+  const { id: urlID } = useParams<WorkDetailParams>();
+  const initialSelectedTabId = urlID ?? listConfig[0].id;
   return (
     <div
       style={{
