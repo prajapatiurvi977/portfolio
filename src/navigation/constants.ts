b97d4a66a16a2ad691997570710ddc3dfd8b6256
prajapatiurvi977 <prajapatiurvi977@gmail.com>
2024-05-11 16:59:52 -0400

    Bypass build error

--- src/navigation/constants.ts
+++ src/navigation/constants.ts
@@ -1,16 +1,3 @@
-const expandRoutes = <T extends Record<string, any>>(routes: T, root = '') => {
-  for (const [key, route] of Object.entries(routes)) {
-    if (typeof route === 'string') {
-      if (key === 'ROOT') continue;
-      routes[key] = [routes.ROOT, route].join('/');
-    } else {
-      route.ROOT = [routes.ROOT, route.ROOT].join('/');
-      expandRoutes(route);
-    }
-  }
-  return routes;
-};
-
 const ROUTE_NAMES = {
   ROOT: '/',
   WORK_DETAIL: {
@@ -20,6 +7,22 @@ const ROUTE_NAMES = {
     HUMBER_CURRENT: 'humber-current',
     TIC_TAC_TOE: 'tic-tac-toe',
   },
+};
+
+type RouteNameType = typeof ROUTE_NAMES;
+
+const expandRoutes = <T extends Partial<RouteNameType>>(routes: T): T => {
+  for (const [key, route] of Object.entries(routes)) {
+    if (typeof route === 'string') {
+      if (key === 'ROOT') continue;
+      // @ts-expect-error Type 'string' is not assignable to type RouteNameKeys;
+      routes[key] = [routes.ROOT, route].join('/');
+    } else {
+      route.ROOT = [routes.ROOT, route.ROOT].join('/');
+      expandRoutes(route);
+    }
+  }
+  return routes;
 };
 
 const ROUTES = expandRoutes(ROUTE_NAMES);
