15b4339ff434cd16af069841dd6dcf0c4e566145
prajapatiurvi977 <prajapatiurvi977@gmail.com>
2024-08-06 21:49:32 -0400

    remove portfolio from router

--- src/navigation/router.tsx
+++ src/navigation/router.tsx
@@ -2,8 +2,6 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import { routes } from './routes';
 
-const router = createBrowserRouter(routes, {
-  basename: '/portfolio/',
-});
+const router = createBrowserRouter(routes);
 
 export { router };
